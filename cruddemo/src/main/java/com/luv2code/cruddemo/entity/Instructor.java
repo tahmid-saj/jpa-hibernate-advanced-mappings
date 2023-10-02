d923143624f164114eae22025fe5f9a816f6c56c
tahmid-saj <tahmid.saj@gmail.com>
2023-10-02 17:30:21 -0400

    1-* fetch types

--- cruddemo/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
+++ cruddemo/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
@@ -40,7 +40,9 @@ public class Instructor {
     @JoinColumn(name="instructor_detail_id")
     private InstructorDetail instructorDetail;
 
-    @OneToMany(mappedBy = "instructor", cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+    @OneToMany(mappedBy = "instructor",
+            fetch = FetchType.EAGER,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE,
             CascadeType.DETACH, CascadeType.REFRESH})
     private List<Course> courses;
 
