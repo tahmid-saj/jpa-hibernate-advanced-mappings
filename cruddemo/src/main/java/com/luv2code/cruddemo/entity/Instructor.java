c622acb771ee7b9365014871c4be662588a7dcac
tahmid-saj <tahmid.saj@gmail.com>
2023-10-02 15:52:11 -0400

    1-* mapping

--- cruddemo/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
+++ cruddemo/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
@@ -2,6 +2,9 @@ package com.luv2code.cruddemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="instructor")
 public class Instructor {
@@ -36,6 +39,14 @@ public class Instructor {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name="instructor_detail_id")
     private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor", cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    private List<Course> courses;
+
+    public Instructor() {
+
+    }
 
     public Instructor(String firstName, String lastName, String email) {
         this.firstName = firstName;
@@ -93,4 +104,24 @@ public class Instructor {
                 ", instructorDetail=" + instructorDetail +
                 '}';
     }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    // add convenience methods for bi-directional relationships
+    public void add(Course tempCourse) {
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+
+        tempCourse.setInstructor(this);
+    }
+
 }
