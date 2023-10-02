9d018addd17b399168a24c2a90b17efd7ef52d0b
tahmid-saj <tahmid.saj@gmail.com>
2023-10-02 14:18:36 -0400

    bi-directional 1-1

--- cruddemo/src/main/java/com/luv2code/cruddemo/entity/InstructorDetail.java
+++ cruddemo/src/main/java/com/luv2code/cruddemo/entity/InstructorDetail.java
@@ -28,6 +28,10 @@ public class InstructorDetail {
     @Column(name="hobby")
     private String hobby;
 
+    // add @OneToOne annotation
+    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+    private Instructor instructor;
+
     public InstructorDetail() {
 
     }
@@ -59,6 +63,14 @@ public class InstructorDetail {
 
     public void setHobby(String hobby) {
         this.hobby = hobby;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
     }
 
     @Override
