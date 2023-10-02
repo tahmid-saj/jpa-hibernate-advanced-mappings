db6896fa700c11a6d86357629b6274fe6cb280ab
tahmid-saj <tahmid.saj@gmail.com>
2023-10-02 15:43:47 -0400

    1-* mapping

--- cruddemo/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ cruddemo/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -1,7 +1,6 @@
 package com.luv2code.cruddemo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="course")
@@ -17,11 +16,19 @@ public class Course {
 
     // annotate fields
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "title")
     private String title;
 
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+                CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
 
     public Course() {
 
